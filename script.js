6d9eb8541789646fd9f14c6acfe54b6bc8535418
Bárbara Gabriela <barbaragabi15@hotmail.com>
2021-11-25 22:33:02 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,43 @@
+
+const calcular = document.getElementById('calcular');
+
+
+calcular.addEventListener('click', imc);
+
+function imc (){
+
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+    
+    if(nome !== ''  && altura !== '' && peso !== ''){
+
+        const valorImc= (peso/(altura*altura)).toFixed(1);
+        
+        let classificacao = '';
+
+        if (valorImc < 18.5){
+            classificacao = "abaixo do peso";
+        }
+        else if (valorImc < 25){
+            classificacao = "com o peso ideal";
+        }
+        else if (valorImc < 30){
+            classificacao = "levemente acima do peso";
+        }
+        else if (valorImc < 35){
+            classificacao = "com obesidade grau 1";
+        }
+        else if (valorImc < 40){
+            classificacao = "com obesidade grau 2";
+        }
+        else{
+            classificacao = "com obesidade grau 3";
+        }
+        resultado.textContent = `${nome} seu IMC é ${valorImc} e você está ${classificacao}`
+    }
+    else{
+        resultado.textContent = 'preencha todos os campos'
+    }
+}
